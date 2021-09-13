bf310e940ffc453aebfc72314955130eac736564
Jill <jilltu330@gmail.com>
2021-09-13 23:05:47 +0800

    feat: add Login.vue ui & route

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import AdminLogin from "../views/AdminLogin.vue";
+import Login from "../views/Login.vue";
 import NotFound from "../views/NotFound.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
   {
     path: "/admin",
     name: "admin",
