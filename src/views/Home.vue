87216e603c136cc24d60a98e685c93324dd1a4da
shichia22 <shichia0207@gmail.com>
2021-09-14 18:36:58 +0800

    feat: NavBar components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>Home</h1>
+  <div id="home">
+      <div class="navbar">
+        <NavBars />
+      </div>
+      <div class="home-center">中</div>
+      <div class="nome-right">右</div>
+  </div>
 </template>
+
+<script>
+import NavBars from './../components/NavBars.vue'
+
+export default {
+  components: {
+    NavBars,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#home {
+  width: 100%;
+  display: flex;
+}
+
+.home-center {
+  flex-grow: 1;
+}
+
+.nome-right {
+  max-width: 350px;
+}
+</style>
