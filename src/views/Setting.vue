444fe45f3dac7f006e3eb6c9f99871e7fa3e6584
shichia22 <shichia0207@gmail.com>
2021-09-15 21:28:11 +0800

    feat: setting form

---
+++ src/views/Setting.vue
@@ -0,0 +1,205 @@
+<template>
+  <div id="setting">
+    <div class="navbar">
+      <NavBars :navItems="navItems" />
+    </div>
+    <div class="setting-right">
+      <form @submit.prevent.stop="handleSubmit">
+        <div class="form-label-group">
+          <label for="account">帳號</label>
+          <input
+            id="account"
+            v-model="account"
+            name="account"
+            type="text"
+            class="form-control"
+            autocomplete="username"
+            placeholder="帳號"
+            required
+            autofocus
+          />
+        </div>
+
+        <div class="form-label-group">
+          <label for="name">名稱</label>
+          <input
+            id="name"
+            v-model="name"
+            name="name"
+            type="text"
+            class="form-control"
+            autocomplete="username"
+            placeholder="名稱"
+            required
+            autofocus
+          />
+        </div>
+
+        <div class="form-label-group">
+          <label for="email">Email</label>
+          <input
+            id="email"
+            v-model="email"
+            name="email"
+            type="email"
+            class="form-control"
+            autocomplete="email"
+            placeholder="email"
+            required
+          />
+        </div>
+
+        <div class="form-label-group">
+          <label for="password">密碼</label>
+          <input
+            id="password"
+            v-model="password"
+            name="password"
+            type="password"
+            class="form-control"
+            placeholder="密碼"
+            autocomplete="new-password"
+            required
+          />
+        </div>
+
+        <div class="form-label-group">
+          <label for="password-check">密碼確認</label>
+          <input
+            id="password-check"
+            v-model="passwordCheck"
+            name="passwordCheck"
+            type="password"
+            class="form-control"
+            placeholder="密碼確認"
+            autocomplete="new-password"
+            required
+          />
+        </div>
+        <!-- TODO:完成儲存按鈕靠右 -->
+        <button class="btn" type="submit">儲存</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBars from '../components/NavBars.vue'
+
+export default {
+  components: {
+    NavBars,
+  },
+  data() {
+    return {
+      navItems: [
+        {
+          name: 'home',
+          text: '首頁',
+          to: '/',
+          icon: () => import('@/components/icon/NavHome.vue'),
+        },
+        {
+          name: 'profile',
+          text: '個人資料',
+          to: '/users/:userid/profile',
+          icon: () => import('@/components/icon/NavProfile.vue'),
+        },
+        {
+          name: 'setting',
+          text: '設定',
+          to: '/setting',
+          icon: () => import('@/components/icon/NavSetting.vue'),
+        },
+      ],
+      user: {
+        account: '',
+        name: '',
+        email: '',
+        password: '',
+        passwordCheck: '',
+      },
+    }
+  },
+  methods: {
+    handleSubmit() {
+      const data = JSON.stringify({
+        account: this.account,
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        passwordCheck: this.passwordCheck,
+      })
+      // TODO: 向後端驗證使用者登入資訊是否合法
+      console.log('data', data)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#setting {
+  width: 100%;
+  display: flex;
+  max-width: 1245px;
+}
+
+.setting-right {
+  margin-left: 2.5rem;
+  padding: 1.875rem 0 0 1rem;
+  flex-grow: 1;
+  border-left: 1px solid $popular-border;
+}
+
+.form-label-group {
+  position: relative;
+  label {
+    position: absolute;
+    left: 0.625rem;
+    top: 0.313rem;
+    padding-bottom: 2em;
+    color: $input-placeholder;
+    font-size: 15px;
+    font-weight: 500;
+    line-height: 15px;
+  }
+  input {
+    width: 40.125rem;
+    height: 3.25rem;
+    border-radius: 4px;
+    background-color: $input-bg;
+    padding-top: 1.25rem;
+    padding-bottom: 0.25rem;
+    margin-bottom: 2em;
+    color: $main-text;
+        font-size: 19px;
+    font-weight: 500;
+    @extend %form-input-style;
+    // TODO:待確認紅線
+    &:not(:placeholder-shown):invalid {
+      border-bottom: 3px solid $input-underline-error;
+    }
+
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0px 1000px $input-bg inset;
+      box-shadow: 0 0 0px 1000px $input-bg inset;
+    }
+    &::placeholder {
+      color: transparent;
+    }
+    &:not(:placeholder-shown):invalid {
+      border-bottom: 3px solid $input-underline-error;
+    }
+  }
+}
+
+.btn {
+  border-radius: 50px;
+  background-color: $button-color;
+  font-size: 18px;
+  font-weight: 700;
+  color: $button-text;
+  margin-top: 0.625rem;
+  padding: 0.625rem 2.5rem;
+}
+</style>
