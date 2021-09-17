058f0261f253d21208f340a413a1ee92bd4f24c9
shichia22 <shichia0207@gmail.com>
2021-09-17 22:42:31 +0800

    feat: 新增 fontawesome 套件 + main.js 設定

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faEnvelope)
 
 Vue.config.productionTip = false;
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
