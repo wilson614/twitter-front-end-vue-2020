5df0c3cbcec59a9eadfd47fc89ca4cd0527ed449
shichia22 <shichia0207@gmail.com>
2021-09-17 17:11:02 +0800

    feat: 新增 Tweets page

---
+++ src/views/Tweets.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="home">
+    <div class="navbar">
+      <h1>{{ $route.params.id }}</h1>
+      <NavBars/>
+    </div>
+    <div class="home-center">
+      <div class="home-center-tweet"></div>
+      <div class="home-center-tweets">
+      </div>
+    </div>
+    <div class="home-right">
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBars from './../components/NavBars.vue'
+
+export default {
+  components: {
+    NavBars,
+  },
+}
+</script>
