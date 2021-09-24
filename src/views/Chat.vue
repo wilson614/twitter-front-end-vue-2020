ecc48efdca0418c0d2938a46ef92718aa817795f
shichia22 <shichia0207@gmail.com>
2021-09-24 21:19:17 +0800

    feat: 新增挑戰功能- 公開聊天室

---
+++ src/views/Chat.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="navbar">
+      <NavBars/>
+    </div>
+    <div class="home-center">
+      <NavTabs plainText="首頁" />
+      <div class="home-center-tweet">
+      </div>
+          <div class="home-right"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBars from './../components/NavBars.vue'
+import NavTabs from '../components/NavTabs.vue'
+
+
+export default {
+  name: 'Chat',
+  components: {
+    NavBars,
+    NavTabs
+  },
+}
+</script>
