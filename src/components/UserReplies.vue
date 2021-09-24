da8efb1ac0474b7ed34a1918d11db467e0a55ca2
shichia22 <shichia0207@gmail.com>
2021-09-24 17:24:26 +0800

    fix: 移除 UserReplies slice

--- src/components/UserReplies.vue
+++ src/components/UserReplies.vue
@@ -24,7 +24,7 @@
               >{{ reply.Tweet.User.account }}</router-link
             >
           </div>
-          <p class="reply-content">{{ reply.comment.slice(0, 140)+'.' }}</p>
+          <p class="reply-content">{{ reply.comment }}</p>
         </div>
       </li>
     </ul>
